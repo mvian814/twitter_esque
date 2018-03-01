27ec3f92a1af10572a2d63fe53bd8e9497aebbed
Maria Vianello <mvian814@gmail.com>
2018-02-28 20:29:22 -0500

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  def full_name
+    "#{first_name} #{last_name}"
+
+  end
+
+  def location
+    "#{city}, #{state}"
+  end
+
+
+
+
+
+end
---
+++ db/migrate/20180301000715_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :bio
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
